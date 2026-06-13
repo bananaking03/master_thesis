d5150b010c208c1bfb75311b5c72eb9d4ab21692
Sam <sambuydens03@gmail.com>
2026-06-13 14:51:33 +0200

    added histogram in sh file

--- run/digital/test_calibration.sh
+++ run/digital/test_calibration.sh
@@ -5,5 +5,5 @@ xrun \
 	$WORK_DIR/verilog/tb/calibration_module_tb.v \
 	$WORK_DIR/verilog/rtl/calibration_module.sv \
 	$WORK_DIR/verilog/rtl/dither_gen.v \
-	$WORK_DIR/verilog/rtl/dither_gen.v \
+	$WORK_DIR/verilog/rtl/histogram.v \
 	
