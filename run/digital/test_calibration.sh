2c1c7b0d85b895d3c0e1a85981e4ecf1e427b7b8
Sam <sambuydens03@gmail.com>
2026-06-13 14:22:39 +0200

    changed to sv in run file

--- run/digital/test_calibration.sh
+++ run/digital/test_calibration.sh
@@ -3,7 +3,7 @@ xrun \
 	-debug \
 	-linebug \
 	$WORK_DIR/verilog/tb/calibration_module_tb.v \
-	$WORK_DIR/verilog/rtl/calibration_module.v \
+	$WORK_DIR/verilog/rtl/calibration_module.sv \
 	$WORK_DIR/verilog/rtl/dither_gen.v \
 	$WORK_DIR/verilog/rtl/dither_gen.v \
 	
